b80a17b4670e4d2119cbcd01eacc87b60aca066b
MNIB <griffe_b@epita.fr>
2016-05-17 22:00:28 +0200

    added comments

--- Projet.c
+++ Projet.c
@@ -15,6 +15,7 @@ typedef int bool;
 #define true 1
 #define false 0
 
+/* Defining type for game board/player */
 typedef struct T_jeu T_jeu;
 struct T_jeu
 {
@@ -22,6 +23,7 @@ struct T_jeu
   T_domino *allTiles;
 };
 
+/*Type for tiles */
 T_domino newT_domino(int _val1, int _val2)
 {
   T_domino tile;
@@ -32,6 +34,7 @@ T_domino newT_domino(int _val1, int _val2)
   return tile;
 }
 
+/* Siple function generating tiles */
 T_domino* generateDominos(const int nbTiles)
 {
   T_domino *tiles = malloc(nbTiles * sizeof(T_domino));
@@ -48,34 +51,36 @@ T_domino* generateDominos(const int nbTiles)
   return tiles;
 }
 
+/* Printing function */
 void printDomino(T_domino tile)
 {
   printf(" _\n|\033[4m%d\033[24m|\n|\033[4m%d\033[24m|\n", tile.val1, tile.val2);
 }
 
+/* Simple function to copy a tiles array */
 T_domino* arrayCopy(T_domino* baseArray, int len, int newDispo)
 {
   T_domino *copy = malloc(len * sizeof(T_domino));
   for (int i = 0; i < len; i++)
   {
-    //printf("At %d, base {%d, %d}\n", i, baseArray[i].val1, baseArray[i].val2);
     copy[i] = baseArray[i];
-    //printf("At %d, base {%d, %d}\n", i, copy[i].val1, copy[i].val2);
     copy[i].dispo = newDispo;
   }
 
   return copy;
 }
 
+/* Function that print available tiles for player or board */
 void printAvailableTiles(T_domino* tiles, int len)
 {
   for (int i = 0; i < len; i++)
   {
     if(tiles[i].dispo == 1)
-      printDomino(tiles[i]);
+    printDomino(tiles[i]);
   }
 }
 
+/* Function to check if the tile is available */
 bool arrayContains(int* array, int value, int len)
 {
   int i = 0;
@@ -84,7 +89,7 @@ bool arrayContains(int* array, int value, int len)
   while(i < len && contains == false)
   {
     if(array[i] == value)
-      contains = true;
+    contains = true;
 
     i++;
   }
@@ -92,6 +97,7 @@ bool arrayContains(int* array, int value, int len)
   return contains;
 }
 
+/* Creating a random array of size arg(size) and within range arg(range) */
 int* randomNbArray(int size, int maxRange)
 {
   int *elements = malloc(sizeof(int)*size);
@@ -118,55 +124,62 @@ int* randomNbArray(int size, int maxRange)
   return elements;
 }
 
+/* Main menu for player p */
+void PrintMenu() //TODO : add args for current player
+{
+  printf("Hello player !\n"
+          "What do you want to do?\n\n"
+          "\t1.Place tile"
+          "\t2.Exit\n\n > ");
+
+  getchar();
+}
+
 int main(int argc , char * argv[])
-	{
-		/* Game variables */
-		const int NB_DOM = 28;
-    const int NB_INITIAL_DOM = 6;
-    const int NB_JOUEURS = 1;
+{
+  /* Game variables */
+  const int NB_DOM = 28;
+  const int NB_INITIAL_DOM = 6;
+  const int NB_JOUEURS = 1;
+  T_domino *tilesList = generateDominos(NB_DOM);
+  /* Creating game board */
+  T_jeu board;
+  board.nbTilesLeft = NB_DOM;
+  board.allTiles = tilesList;
 
-    /* Creating list of tiles */
-    T_domino *tilesList = generateDominos(NB_DOM);
+  T_jeu* players = malloc((NB_JOUEURS + 1) * sizeof(T_jeu));
 
-    /* Creating game board */
-    T_jeu board;
-    board.nbTilesLeft = NB_DOM;
-    board.allTiles = tilesList;
+  /* Initializing rand function */
+  srand((int)time(NULL));
 
-    T_jeu* players = malloc((NB_JOUEURS + 1) * sizeof(T_jeu));
+  for (int i = 0; i <= NB_JOUEURS; i++)
+  {
+    players[i].nbTilesLeft = NB_INITIAL_DOM;
+    players[i].allTiles = arrayCopy(tilesList, NB_DOM, 0);
+    int *randomNumbers = randomNbArray(NB_INITIAL_DOM, NB_DOM);
 
-    /* Initializing rand function */
-    srand((int)time(NULL));
-
-    for (int i = 0; i <= NB_JOUEURS; i++)
+    for (int j = 0; j < NB_INITIAL_DOM; j++)
     {
-      players[i].nbTilesLeft = NB_INITIAL_DOM;
-      //memcpy(players[i].allTiles, tilesList, NB_INITIAL_DOM);
-      players[i].allTiles = arrayCopy(tilesList, NB_DOM, 0);
-      int *randomNumbers = randomNbArray(NB_INITIAL_DOM, NB_DOM);
-
-      for (int j = 0; j < NB_INITIAL_DOM; j++)
-      {
-        //printf("%d : %d, %d\n", randomNumbers[j], players[i].allTiles[randomNumbers[j]].val1, players[i].allTiles[randomNumbers[j]].val1);
-        players[i].allTiles[randomNumbers[j]].dispo = 1;
-      }
+      players[i].allTiles[randomNumbers[j]].dispo = 1;
     }
+  }
 
-    /* Printing player available tiles */
-    for (int i = 0; i <= NB_JOUEURS; i++)
-    {
-      printf("Player %d :\n", i+1);
-      printAvailableTiles(players[i].allTiles, NB_DOM);
-    }
+  /* Printing player available tiles */
+  for (int i = 0; i <= NB_JOUEURS; i++)
+  {
+    printf("Player %d :\n", i+1);
+    printAvailableTiles(players[i].allTiles, NB_DOM);
+  }
 
-		GetKey();
+  /* Printing menu */
+  PrintMenu();
 
-    /* REGION : Freeing memory */
-    free(players[0].allTiles);
-    free(players[1].allTiles);
-    free(tilesList);
-    free(players);
-    /* ENDREGION */
+  /* REGION : Freeing memory */
+  free(players[0].allTiles); //TODO : Un-hardcode this shit
+  free(players[1].allTiles); //TODO : Un-hardcode this shit
+  free(tilesList);
+  free(players);
+  /* ENDREGION */
 
-		return 0;
-	}
+  return 0;
+}
